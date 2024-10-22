2bff33db5f02a2126072ba9b0b21004dbde5f728
Tiago Teague Violante <339784910@GoTVDSB.ca>
2024-10-22 08:41:04 -0400

    Add project files.

---
+++ Topic-9 mini calculator/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Topic_9_mini_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnDivid = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.lblAnswer1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.AccessibleRole = System.Windows.Forms.AccessibleRole.Sound;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(12, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(53, 51);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSub.Location = new System.Drawing.Point(12, 69);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(53, 51);
+            this.btnSub.TabIndex = 1;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnDivid
+            // 
+            this.btnDivid.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivid.Location = new System.Drawing.Point(12, 126);
+            this.btnDivid.Name = "btnDivid";
+            this.btnDivid.Size = new System.Drawing.Size(53, 51);
+            this.btnDivid.TabIndex = 2;
+            this.btnDivid.Text = "÷";
+            this.btnDivid.UseVisualStyleBackColor = true;
+            this.btnDivid.Click += new System.EventHandler(this.btnDivid_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiply.Location = new System.Drawing.Point(12, 183);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(53, 51);
+            this.btnMultiply.TabIndex = 3;
+            this.btnMultiply.Text = "×";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // lblAnswer1
+            // 
+            this.lblAnswer1.AutoSize = true;
+            this.lblAnswer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer1.Location = new System.Drawing.Point(89, 194);
+            this.lblAnswer1.Name = "lblAnswer1";
+            this.lblAnswer1.Size = new System.Drawing.Size(27, 29);
+            this.lblAnswer1.TabIndex = 4;
+            this.lblAnswer1.Text = "0";
+            this.lblAnswer1.Click += new System.EventHandler(this.lblAnswer1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 254);
+            this.Controls.Add(this.lblAnswer1);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnDivid);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnDivid;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Label lblAnswer1;
+    }
+}
+
---
+++ Topic-9 mini calculator/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Topic_9_mini_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblAnswer1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lblAnswer1.Text = "8 * 8 = " + (8 * 8);
+            btnAdd.Enabled = false;
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            lblAnswer1.Text = "8 * 8 = " + (8 * 8);
+            btnSub.Enabled = false;
+        }
+
+        private void btnDivid_Click(object sender, EventArgs e)
+        {
+            lblAnswer1.Text = "8 * 8 = " + (8 * 8);
+            btnDivid.Enabled = false;
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            lblAnswer1.Text = "8 * 8 = " + (8 * 8);
+            btnMultiply.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
